27ad50e204988f99efe6f88e4e06f2960b4ce156
nukrisnamurti <ruciera@gmail.com>
2022-09-22 14:14:08 +0700

    User page refinement - 01

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -17,62 +17,62 @@ function Card() {
       </section>
       {/* Insert Filter Job */}
 
-      <section>
-      <div className="container mx-auto ">
-            <table className=" leading-normal table-auto text-sm text-left text-gray-500 mx-auto">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 text-center">
-                    <tr>
-                        
-                        <th className="px-6 py-3">Title</th>
-                        {/* <th className="px-6 py-3">Description</th> */}
-                        <th className="px-6 py-3">Company</th>
-                        {/* <th className="px-6 py-3">Location</th> */}
-                        {/* <th className="px-6 py-3">Salary</th> */}
-                        <th className="px-6 py-3">Type</th>
-                        <th className="px-6 py-3">Experience Level</th>
-                        {/* <th className="px-6 py-3">Skills</th> */}
-                        <th className="px-6 py-3">Valid Until</th>
-                        <th className="px-6 py-3">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                        
-                            <tr 
-                            // key={index} 
-                            className="bg-white border-b hover:bg-gray-50 text-center">
-                                
-                                <td className="px-6 py-4 font-medium"> role </td>
-                                
-                                <td className="px-6 py-4">
-                                  {/* {shortenText(res.job_qualification, 10)} */}
-                                  Company
-                                  </td>
-                                
-                                <td className="px-6 py-4">
-                                  {/* {jobStatus(res.job_status)} */}
-                                  Type
-                                  </td>
-                                <td className="px-6 py-4">Experience</td>
-                                
-                                <td className="px-6 py-4">Closing date</td>
+      <section class="container">
+        <div className="justify-around flex">
+          <table className="leading-normal table-auto text-sm text-gray-500 flex-row flex-[100vh]">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 text-center  ">
+              <tr>
+                <th className="px-6 py-3">Title</th>
+                {/* <th className="px-6 py-3">Description</th> */}
+                <th className="px-6 py-3">Company</th>
+                {/* <th className="px-6 py-3">Location</th> */}
+                {/* <th className="px-6 py-3">Salary</th> */}
+                <th className="px-6 py-3">Type</th>
+                <th className="px-6 py-3">Experience Level</th>
+                {/* <th className="px-6 py-3">Skills</th> */}
+                <th className="px-6 py-3">Valid Until</th>
+                <th className="px-6 py-3">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                // key={index}
+                className="bg-white border-b hover:bg-gray-50 text-center"
+              >
+                <td className="px-6 py-4 font-medium"> role </td>
 
-                                <td className="px-6 py-4"> 
-                                <Link to='/user/jobs/jobId'>
-                                <button 
-                                // onClick={handleEdit} 
-                                // value={res.id} 
-                                className="font-medium text-blue-600 hover:underline">Details</button>
-                                </Link>
-                                    {/* <button 
+                <td className="px-6 py-4">
+                  {/* {shortenText(res.job_qualification, 10)} */}
+                  Company
+                </td>
+
+                <td className="px-6 py-4">
+                  {/* {jobStatus(res.job_status)} */}
+                  Type
+                </td>
+                <td className="px-6 py-4">Experience</td>
+
+                <td className="px-6 py-4">Closing date</td>
+
+                <td className="px-6 py-4">
+                  <Link to="/user/jobs/jobId">
+                    <button
+                      // onClick={handleEdit}
+                      // value={res.id}
+                      className="font-medium text-blue-600 hover:underline"
+                    >
+                      Details
+                    </button>
+                  </Link>
+                  {/* <button 
                                     onClick={handleDelete} 
                                     value={res.id} 
                                     className="font-medium text-red-600 hover:underline">Delete</button> */}
-                                </td>
-                            </tr>
-                </tbody>
-            </table>
-            </div>
-
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </section>
     </>
   );
--- src/pages/user/application.js
+++ src/pages/user/application.js
@@ -1,43 +1,41 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar/navbar";
 
 const Application = () => {
   return (
     <div>
-
-        <section>
-        <div className="container mx-auto ">
-            <table className=" leading-normal table-auto text-sm text-left text-gray-500 mx-auto">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 text-center">
-                    <tr>
-                        
-                        <th className="px-6 py-3">Applied On</th>
-                        <th className="px-6 py-3">Role Applied</th>
-                        <th className="px-6 py-3">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                        
-                            <tr 
-                            // key={index} 
-                            className="bg-white border-b hover:bg-gray-50 text-center">
-                                
-                                <td className="px-6 py-4 font-medium"> Date Applied </td>
-                                <td className="px-6 py-4">
-                                  {/* {shortenText(res.job_description, 10)} */}
-                                  System Application Developer
-                                  </td>
-                                <td className="px-6 py-4">
-                                  {/* {shortenText(res.job_qualification, 10)} */}
-                                  Status of Application
-                                  </td>
-                            </tr>
-                </tbody>
-            </table>
-            </div>
-        </section>
-
+      <Navbar />
+      <section className="container my-9">
+        <div className="justify-around flex ">
+          <table className=" leading-normal table-auto text-sm text-gray-500 flex-row flex-[100vh]">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 text-center">
+              <tr>
+                <th className="px-6 py-3">Applied On</th>
+                <th className="px-6 py-3">Role Applied</th>
+                <th className="px-6 py-3">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                // key={index}
+                className="bg-white border-b hover:bg-gray-50 text-center"
+              >
+                <td className="px-6 py-4 font-medium"> Date Applied </td>
+                <td className="px-6 py-4">
+                  {/* {shortenText(res.job_description, 10)} */}
+                  System Application Developer
+                </td>
+                <td className="px-6 py-4">
+                  {/* {shortenText(res.job_qualification, 10)} */}
+                  Status of Application
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Application
+export default Application;
